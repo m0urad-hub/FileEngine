73be594860a739550e2161a5d921b3392e1a13a5
mourad ayadi <mouradayadi7035@gmail.com>
2026-03-11 04:13:09 +0100

    dev database'

--- file_engine.py
+++ file_engine.py
@@ -1,30 +1,75 @@
 from pathlib import Path
 import time
-def info(file):
-    name = file.stem
-    size = file.stat().st_size # size in bytes
-    ctime = time.ctime(file.stat().st_ctime) # creation time
-    mtime = time.ctime(file.stat().st_mtime) # last modified time
+import sqlite3
+import datetime 
+conn = sqlite3.connect("file_engine_storage.db")
+cursor = conn.cursor()
+#pagma foreign_keys =on
 
-    print(f"Name: {name}")
-    print(f"Extension: {extension}")
-    print(f"Size: {size} bytes")
-    print(f"Created: {ctime}")
-    print(f"Modified: {mtime}")
-    print("-" * 30)
-path=input("give")
-folder = Path(path)
-print(folder)
-type=input("give type of files")
-stat_size =int(input("give minim size "))
-for file in folder.rglob("*"): 
-    if file.is_file():
-        extension = file.suffix
-        size = file.stat().st_size
-        if stat_size<size :
-            info(file)
-        #if extension==type :
-            #info(file)
+
+cursor.execute("""create table if not exists file(
+id_file integer primary key autoincrement,
+path text unique,
+lasts_updated datetime
+)""")
+
+
+def insert_file(cursor):
+    path=input("give")
+    date=datetime.datetime.now().strftime("%Y-%m-%d")
+    folder = Path(path)
+    try :
+        cursor.execute("insert into file (path,lasts_updated) values(?,?)",(path,date))
+        conn.commit()
+    except Exception as e:
+        print(e)
+
+
+
+
+cursor.execute("""create table if not exists info(
+id_info integer primary key,
+id_file integer ,
+file_name text,
+extension text,
+size integer,
+created_at datetime,
+modified_at datetime,
+foreign key(id_file) references file(id_file))   """)
+
+
+def insert_info(cursor,folder_p):
+    cursor.execute("select * from file ")
+    data=cursor.fetchall()
+    for column in data:
+        if column[1]==folder_p :
+            print("path alredy exist")
+        else :
+            f=Path(column[1])
+            folder_id=column[0]
+            for file in f.rglob("*"): 
+                if file.is_file():
+                    extension = file.suffix
+                    size = file.stat().st_size
+                    name = file.stem
+                    size = file.stat().st_size # size in bytes
+                    ctime = time.ctime(file.stat().st_ctime) # creation time
+                    mtime = time.ctime(file.stat().st_mtime) # last modified time
+                    cursor.execute("insert into info (file_name,extension,size,created_at,modified_at,id_file) values(?,?,?,?,?,?)",(name,extension,size,ctime,mtime,folder_id))
+                    break 
+            
+
+  
+insert_file(cursor)
+folder_p=input("give intended folder path")
+insert_info(cursor,folder_p)
+cursor.execute("select*from file")
+print(cursor.fetchall())
+
+    
+  
+
+
         
 
         
